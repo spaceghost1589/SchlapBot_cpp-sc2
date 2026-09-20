aa0b275cdcde85505e5e12a56a1e892bda0140ba
spaceghost1589 <sean.egan27@gmail.com>
2026-09-20 16:16:20 -0700

    proto2 to proto3 added `0` enums spelling

--- thirdparty/sc2protocol/sc2protocol.cmake
+++ thirdparty/sc2protocol/sc2protocol.cmake
@@ -5,12 +5,7 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/Blizzard/s2client-proto.git
     GIT_TAG bff45dae1fc685e6acbaae084670afb7d1c0832c
     GIT_PROGRESS TRUE
-    # Patch to apply:
-    # - Packed Encoding for Repeated Fields
-    # - Arena Allocation (requires local implementation)
-    PATCH_COMMAND git apply
-        --ignore-space-change
-        --ignore-whitespace
-        "${CMAKE_CURRENT_LIST_DIR}/sc2protocol-arena-packing.patch"
+    UPDATE_DISCONNECTED TRUE
+    PATCH_COMMAND git apply --ignore-space-change --ignore-whitespace "${CMAKE_CURRENT_LIST_DIR}/proto3.patch"
 )
 FetchContent_MakeAvailable(sc2protocol)
